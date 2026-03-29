9199e64e2197202bcd088ecd49cdcf26998e1f9c
Mireille <virtuekyoko@gmail.com>
2026-03-29 20:22:46 +0800

    Update card_data.js

--- card_data.js
+++ card_data.js
@@ -1,12 +1,9 @@
-// card_data.js
-let MY_CARD_DATA = [
-    { id:0, name:"現金(早餐)", cls:"cash", s:1, d:31, type:"cash", details:[], paid: false },
-    { id:1, name:"中信", cls:"ctbc", s:22, d:8, details:[], paid: false },
-    // ... 依此類推
-];
+// card_data.js 內容只要這一段就好
 const MY_CARD_DATA = [
   { "id": 0, "name": "現金(早餐)", "cls": "cash", "s": 1, "d": 31, "type": "cash", "details": [], "paid": false },
   { "id": 1, "name": "中信", "cls": "ctbc", "s": 22, "d": 8, "details": [], "paid": false },
-  // ... 剩下的卡片資料
+  { "id": 2, "name": "台新", "cls": "ts", "s": 2, "d": 17, "details": [], "paid": false },
+  { "id": 3, "name": "玉山", "cls": "esun", "s": 13, "d": 28, "details": [], "paid": false },
+  { "id": 4, "name": "合庫", "cls": "tcb", "s": 1, "d": 15, "details": [], "paid": false },
+  { "id": 5, "name": "星展", "cls": "dbs", "s": 25, "d": 13, "details": [], "paid": false }
 ];
-
