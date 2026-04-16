12e79c6019ae5956cc7694984e26f24eeffc136f
Mireille <virtuekyoko@gmail.com>
2026-04-16 21:13:21 +0800

    Update card_data.js

--- card_data.js
+++ card_data.js
@@ -1,9 +1,9 @@
 // card_data.js 內容只要這一段就好
 const MY_CARD_DATA = [
-{"id":0,"name":"現金(早餐)","cls":"cash","s":1,"d":31,"type":"cash","details":[],"paid":false},
-{"id":1,"name":"中信","cls":"ctbc","s":22,"d":8,"details":[{"date":"03/29","note":"登山包","amt":558}],"paid":false},
-{"id":2,"name":"台新","cls":"ts","s":2,"d":17,"details":[{"date":"03/29","note":"未出帳","amt":8779}],"paid":false},
-{"id":3,"name":"玉山","cls":"esun","s":13,"d":28,"details":[{"date":"03/29","note":"未出帳","amt":2484}],"paid":false},
-{"id":4,"name":"合庫","cls":"tcb","s":1,"d":15,"details":[{"date":"03/29","note":"未出帳","amt":2095}],"paid":false},
-{"id":5,"name":"星展","cls":"dbs","s":25,"d":13,"details":[{"date":"03/29","note":"未出帳","amt":969}],"paid":false}
+  {"id":0,"name":"現金(早餐)","cls":"cash","s":1,"d":31,"type":"cash","details":[],"paid":false},
+  {"id":1,"name":"中信","cls":"ctbc","s":22,"d":8,"details":[{"date":"03/29","note":"登山包","amt":558}],"paid":false},
+  {"id":2,"name":"台新","cls":"ts","s":6,"d":21,"details":[],"paid":false},
+  {"id":3,"name":"玉山","cls":"esun","s":13,"d":28,"details":[],"paid":false},
+  {"id":4,"name":"合庫","cls":"tcb","s":1,"d":17,"details":[],"paid":false},
+  {"id":5,"name":"星展","cls":"dbs","s":8,"d":27,"details":[],"paid":false}
 ];
